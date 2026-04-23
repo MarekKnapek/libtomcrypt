ecbfe31df5edcdc4ac2701c49085ffae1fe82a9d
Marek Knápek <knapek.mar@gmail.com>
2026-04-23 09:01:00 +0200

    Fix from code review.

--- src/hashes/sha2/sha512_256_desc.c
+++ src/hashes/sha2/sha512_256_desc.c
@@ -27,7 +27,7 @@ const struct ltc_hash_descriptor sha512_256_desc =
     NULL
 };
 
-#if defined LTC_SHA256_X86
+#if defined LTC_SHA512_256_X86
 
 #if !defined (LTC_S_X86_CPUID)
 #define LTC_S_X86_CPUID
@@ -109,7 +109,7 @@ static LTC_INLINE int s_sha512_256_x86_is_supported(void)
     }
     return is_supported;
 }
-#endif /* LTC_SHA256_X86 */
+#endif /* LTC_SHA512_256_X86 */
 
 /**
    Initialize the hash state
